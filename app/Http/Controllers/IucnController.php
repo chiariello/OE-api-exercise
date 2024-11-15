18810a6f36c7042d3c94f2a32ded55a0b957c023
Salvatore Chiariello <salvatore.chiariello@clemps.it>
2024-11-16 00:37:29 +0100

    Created iucnService

--- app/Http/Controllers/IucnController.php
+++ app/Http/Controllers/IucnController.php
@@ -2,37 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\IucnApi;
+use App\Services\IucnService;
 
 class IucnController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(IucnApi $iucnApi)
+    public function __invoke(IucnService $iucnService)
     {
-        // 1
-        $regions = $iucnApi->regions();
-        // 2
-        $randRegion = $regions->random();
-        // 3 - 4
-        $species = $iucnApi->species($randRegion['identifier']);
-        // 5
-        $crSpecies = $species->filter(fn($element) => $element['category'] == 'CR');
-
-        foreach ($crSpecies as $sp) {
-            $conservationMeasures[$sp['taxonid']] = collect($iucnApi->conservationMeasures($sp['taxonid']))->implode('title',
-                ' , ');
-        }
-
-        $crSpecies = $crSpecies->map(function ($element) use ($conservationMeasures) {
-            $element['cm'] = array_key_exists($element['taxonid'],
-                $conservationMeasures) ? $conservationMeasures[$element['taxonid']] : '';
-            return $element;
-        });
-        // 6
-        $mammalSpecies = $species->filter(fn($s) => $s['class_name'] == 'MAMMALIA');
-
+        return $iucnService->crSpecies();
     }
 }
 
